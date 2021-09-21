1bb79eb7453cebe57fb5a30e69e54b618eaaa360
AriefDaffa <ariefkeren76@yahoo.com>
2021-09-21 20:29:07 +0700

    edit dashboard menu

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,73 @@
 <div class="content">
     <div class="row">
         <div class="col-lg-12">
-            <div class="card">
+            <div class="row text-center">
+                <div class="col-sm-3">
+                    <div class="card" style="height: 15rem">
+                        Tagihan
+                    </div>
+                </div>
+                <div class="col-sm-3" >
+                    <div class="card"  style="height: 15rem">Menu 1</div>
+                </div>
+                <div class="col-sm-3">
+                    <div class="card"  style="height: 15rem">Menu 2</div>
+                </div>
+                <div class="col-sm-3">
+                    <div class="card"  style="height: 15rem">menu 3</div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-9">
+                    <div class="card">
+                        <div class="bd-example">
+                            <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+                                <ol class="carousel-indicators">
+                                    <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+                                    <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+                                    <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+                                </ol>
+                                <div class="carousel-inner">
+                                    <div class="carousel-item active">
+                                        <img src="https://images.pexels.com/photos/5668808/pexels-photo-5668808.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" class="d-block w-100" alt="gambar" style="height: 30rem">
+                                        <div class="carousel-caption d-none d-md-block">
+                                            <h5>First slide label</h5>
+                                            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                                        </div>
+                                    </div>
+                                    <div class="carousel-item">
+                                        <img src="https://images.pexels.com/photos/4195405/pexels-photo-4195405.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" class="d-block w-100" alt="gambar" style="height: 30rem">
+                                        <div class="carousel-caption d-none d-md-block">
+                                            <h5>Second slide label</h5>
+                                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                                        </div>
+                                    </div>
+                                    <div class="carousel-item">
+                                        <img src="https://images.pexels.com/photos/5668808/pexels-photo-5668808.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" class="d-block w-100" alt="gambar" style="height: 30rem">
+                                        <div class="carousel-caption d-none d-md-block">
+                                            <h5>Third slide label</h5>
+                                            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+                                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                    <span class="sr-only">Previous</span>
+                                </a>
+                                <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+                                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                    <span class="sr-only">Next</span>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="card text-center" style="height: 30rem">Menu lain</div>
+                </div>
+            </div>
+            
+            <!-- <div class="card">
                 <div class="card-header">
                     Dashboard
                 </div>
@@ -17,7 +83,7 @@
 
                     You are logged in!
                 </div>
-            </div>
+            </div> -->
         </div>
     </div>
 </div>
